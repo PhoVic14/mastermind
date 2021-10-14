aa337dbc82464065517e6561e9767f5bd93abcd0
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2021-10-14 14:31:59 +0200

    Add files via upload
    ajout des exercices mastermind

---
+++ mastermind/mastermind/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mastermind
+{
+    class Program
+    {
+        static void mastermind(out int[] combi)
+        {
+            Random rnd = new Random();
+            
+            for (int n = 0; n < 3; n++)
+            {
+                int nombre = rnd.Next(1, 6);
+                combi[n] = nombre;
+            }
+                
+            
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int[] combi;
+
+            Console.WriteLine("Encodez le nombre");
+            combi = int.Parse(Console.ReadLine());
+
+            mastermind(out combi);
+
+            Console.WriteLine($"Le nombre est: {combi}");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
